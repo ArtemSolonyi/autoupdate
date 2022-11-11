56da9ed818a17f5723b142651071f095e53fc96f
ArtemSolonyi <igorartem123@gmail.com>
2022-11-11 22:34:25 +0200

    пе

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { exec } from "child_process";
 
 @Injectable()
 export class AppService {
 
   getHello(): string {
-    return '.huy.';
+    console.log('qqqq')
+    exec("cd C:\\Users\\artem\\WebstormProjects\\autoupdate", (error, stdout, stderr) => {
+      console.log(error)
+      console.log(stdout)
+      exec("git checkout master", (error, stdout, stderr) => {
+        console.log(error,)
+        console.log(stdout,)
+        exec("git pull", () => {
+          exec("npm i", () => {
+            exec("npm start:dev");
+          });
+        });
+      });
+
+
+    });
+
+    return "suc";
   }
 }
