7b8c480775dd6fb367c0a03d3d19b9ce33a1e80a
Klapeks <41518563+Klapeks@users.noreply.github.com>
2022-11-11 22:06:48 +0200

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import axios from 'axios';
 
 describe('AppController', () => {
   let appController: AppController;
